0dab220dc28d49071812eaabbf69101a65f2fb1e
manojsaikumar <manojsaikumar452@gmail.com>
2021-10-26 11:37:14 +0530

    s3

--- aws.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  # access_key = "AWS ACCESS KEY"
-  # secret_key = "AWS SECRET KEY"
-  region     = "us-west-2"
-  # shared_credentials_file = "~/.aws/credentials"
-}
-
--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "manoj_bucket" {
-   count = 3
-   bucket = "${var.s3_bucket_name}${count.index}"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   force_destroy = "true"
-}
