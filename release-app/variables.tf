279e3e07ed7ccbf3f3e0611cdf13fe5b066b8e39
Vedant Pareek <36420365+dunefro@users.noreply.github.com>
2022-11-24 16:56:09 +0530

    Update variables.tf

--- release-app/variables.tf
+++ release-app/variables.tf
@@ -13,7 +13,6 @@ variable "aws_region" {
 variable "http-server" {
     default = {
         chart_version = "0.1.0"
-        image_version = "v0.1.0-rc.6"
-        new_var = "v0"
+        image_version = "v0.1.0-rc.7"
     }
 }
